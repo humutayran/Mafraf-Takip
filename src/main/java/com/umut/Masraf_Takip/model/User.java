a23a024fb3be98abdd2f8ba46f19097eba4f6092
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-07-25 11:17:35 +0300

    transaction controller

--- src/main/java/com/umut/Masraf_Takip/model/User.java
+++ src/main/java/com/umut/Masraf_Takip/model/User.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Getter
@@ -25,4 +28,19 @@ public class User extends BaseEntity {
 
     @Column(name = "EMAIL")
     private String email;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Column(name = "TRANSACTION")
+    private Set<Transaction> transactions;
+
+/*
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+*/
 }

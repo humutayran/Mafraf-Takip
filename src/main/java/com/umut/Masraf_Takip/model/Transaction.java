073cdb9a1995cf39276bb19ad9f45d64152fdea5
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-07-25 10:34:15 +0300

    Transaction modeli

---
+++ src/main/java/com/umut/Masraf_Takip/model/Transaction.java
@@ -0,0 +1,30 @@
+package com.umut.Masraf_Takip.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "TRANSACTIONS")
+public class Transaction extends BaseEntity {
+
+    private String name;
+
+    private BigDecimal amount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID", nullable = false)
+    private User user;
+
+    @Column(name = "TIMESTAMP")
+    private LocalDateTime timestamp;
+}

4aca603ac317ecb5ddea1ac4306310d0f36e1f30
Hüseyin Umut Reyhanoğlu <h.umutayran@gmail.com>
2024-07-25 11:07:24 +0300

    add user endpointi

--- src/main/java/com/umut/Masraf_Takip/controller/UserController.java
+++ src/main/java/com/umut/Masraf_Takip/controller/UserController.java
@@ -1,9 +1,13 @@
 package com.umut.Masraf_Takip.controller;
 
+import com.umut.Masraf_Takip.dto.response.UserResponseDto;
 import com.umut.Masraf_Takip.service.abstraction.UserService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/user")
@@ -15,5 +19,16 @@ public class UserController  {
         this.userService = userService;
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponseDto>> getAllUsers() {
+        List<UserResponseDto> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
 
+
+    @PostMapping
+    public ResponseEntity<UserResponseDto> addUser(@RequestBody UserRequestDto userRequestDto) {
+        UserResponseDto user = userService.addUser(userRequestDto);
+        return new ResponseEntity<>(user, CREATED);
+    }
 }
